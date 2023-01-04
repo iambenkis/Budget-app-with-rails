617ac25c1a2700fee95e841522fdb71bb0671378
benjamin kisenge <benkisenge03@gmail.com>
2023-01-04 14:48:52 +0200

    Style sign up page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  include ImageUploader::Attachment(:image)
+  validates :name, presence: true
 end
