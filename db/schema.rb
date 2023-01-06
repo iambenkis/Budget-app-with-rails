7a3571f2da0bad5bbbb5ab6224071c201582a9fd
benjamin kisenge <benkisenge03@gmail.com>
2023-01-06 16:04:42 +0200

    Create join table category deal

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_06_133135) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_06_140342) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_06_133135) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "categories_deals", id: false, force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "deal_id", null: false
+    t.index ["category_id", "deal_id"], name: "index_categories_deals_on_category_id_and_deal_id"
   end
 
   create_table "deals", force: :cascade do |t|
